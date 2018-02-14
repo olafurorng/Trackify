d6212f8698080820e3e1d9ef761a6aca3b2512d7
Ólafur Örn <olafur@plainvanillagames.com>
2018-02-14 09:57:51 +0100

    Add all the tabs screens

---
+++ app/src/main/java/com/trackify/trackify/SettingsActivity.java
@@ -0,0 +1,39 @@
+package com.trackify.trackify;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        findViewById(R.id.tab_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onTab1Clicked();
+            }
+        });
+        findViewById(R.id.tab_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onTab2Clicked();
+            }
+        });
+    }
+
+    private void onTab1Clicked() {
+        Intent intent = new Intent(this, TodayActivity.class);
+        startActivity(intent);
+    }
+
+    private void onTab2Clicked() {
+        Intent intent = new Intent(this, StatsActivity.class);
+        startActivity(intent);
+    }
+}
